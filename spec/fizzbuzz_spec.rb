6a38ad1b1fc6971e30cb75f1297cdd2d3341ae9b
Soroush Khosravi <sorush.kh68@gmail.com>
2019-01-30 14:43:41 +0000

    rspec file and the main code file are both completed.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,13 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+  it 'returns the number itself when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
 end
